c0b896faed023a018e00118201c27dc7d9145001
tertan41k <paveleremeev856@gmail.com>
2025-10-27 01:23:02 +0500

    add styles

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,95 +37,14 @@ namespace WpfApp_DataBinding_Ver2
             InitializeComponent();
             MainFrame.Navigate(new LoginPage());
 
-
+            ThemeHelper.ApplySavedTheme();
 
         }
 
-        public void Registration(object sender, RoutedEventArgs e)
+        private void ChangeTheme_Click(object sender, RoutedEventArgs e)
         {
-
-  
-                var user = (User)Resources["UserReg"];
-                user.Savetofile();
-                user = null;
-
-            var pacient = (Pacient)Resources["Pacientshow"];
-
-
-        }
-
-        public void Login(object sender, RoutedEventArgs e)
-        {
-          
-            var user=(User)Resources["UserLog"];
-            signin= user.Loadfromfile();
-
-
-            var pacient = (Pacient)Resources["Pacientshow"];
-
-
-
-        }
-
-        public void Addpacient(object sender, RoutedEventArgs e)
-        {
-            var pacient = (Pacient)Resources["Pacientadd"];
-            if (signin)
-            {
-             
-                pacient.Savetofile();
-            }
-            else
-            {
-                MessageBox.Show("Вы не вошли в аккаунт");
-            }
-        }
-
-        public void ShowPacient(object sender, RoutedEventArgs e)
-        {
-    
-            var pacientshow = (Pacient)Resources["Pacientshow"];
-            if (signin)
-            {
-                pacientshow.Loadfromfile();
-            }
-            else
-            {
-                MessageBox.Show("Вы не вошли в аккаунт");
-            }
-        }
-
-        private void Redactpacient(object sender, RoutedEventArgs e)
-        {
-            if (signin)
-            {
-                var pacient = (Pacient)Resources["Pacientshow"];
-                pacient.UpdatePacient();
-            }
-            else
-            {
-                MessageBox.Show("Вы не вошли в аккаунт");
-            }
-
-
-        }
-
-        public void Receptpacient(object sender, RoutedEventArgs e)
-        {
-            if (signin)
-            {
-                var user = (User)Resources["UserLog"];
-                var pacient = (Pacient)Resources["Pacientshow"];
-                pacient.Reception(user.ID);
-            }
-            else
-            {
-                MessageBox.Show("Вы не вошли в аккаунт");
-            }
-
-
-
-        }
+            ThemeHelper.ToggleTheme();
+        }    
 
 
     }
---
+++ ThemeHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using WpfApp_DataBinding_Ver2.Properties;
+
+namespace WpfApp_DataBinding_Ver2
+{
+    public static class ThemeHelper
+    {
+        private static readonly string[] _themePaths = {
+            "/Styles/Colors/DefaultColors.xaml",
+            "/Styles/Colors/DarkTheme.xaml"
+        };
+
+        public static string CurrentTheme
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Settings.Default.ThemePath))
+                {
+                    Settings.Default.ThemePath = _themePaths[0];
+                    Settings.Default.Save();
+                }
+                return Settings.Default.ThemePath;
+            }
+            set
+            {
+                Settings.Default.ThemePath = value;
+                Settings.Default.Save();
+            }
+        }
+
+        public static void ApplyTheme(string themePath)
+        {
+            try
+            {
+                var newTheme = new ResourceDictionary { Source = new Uri(themePath, UriKind.Relative) };
+
+                var app = Application.Current;
+                if (app == null) return;
+
+                // Ищем текущую тему среди словарей цветов
+                var oldTheme = app.Resources.MergedDictionaries
+                    .FirstOrDefault(d => d.Source != null &&
+                                        _themePaths.Any(path =>
+                                            d.Source.OriginalString.EndsWith(path)));
+
+                if (oldTheme != null)
+                {
+                    // Заменяем существующую тему
+                    int index = app.Resources.MergedDictionaries.IndexOf(oldTheme);
+                    app.Resources.MergedDictionaries[index] = newTheme;
+                }
+                else
+                {
+                    // Добавляем новую тему если не найдена
+                    app.Resources.MergedDictionaries.Add(newTheme);
+                }
+
+                CurrentTheme = themePath;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при применении темы: {ex.Message}", "Ошибка",
+                              MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public static void ApplySavedTheme()
+        {
+            ApplyTheme(CurrentTheme);
+        }
+
+        public static void ToggleTheme()
+        {
+            var newTheme = CurrentTheme == _themePaths[0] ? _themePaths[1] : _themePaths[0];
+            ApplyTheme(newTheme);
+        }
+
+        public static void InitializeTheme()
+        {
+            // Применяем сохраненную тему при запуске приложения
+            ApplySavedTheme();
+        }
+    }
+}
